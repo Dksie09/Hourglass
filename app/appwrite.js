a110de6cd5e56f1bca690c697d09543d23c8ce33
Dksie09 <dakshiegoel@gmail.com>
2024-03-02 18:00:04 +0530

    fixed bug

--- app/appwrite.js
+++ app/appwrite.js
@@ -4,8 +4,8 @@ const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(NEXT_PUBLIC_PROJECT_ID);
+    .setProject(process.env.NEXT_PUBLIC_PROJECT_ID);
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
-export const databases = new Databases(client); ``
+export const databases = new Databases(client);
