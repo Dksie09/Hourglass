97baa665e61e16edc4ec186906f0957e6ea0bb96
Dksie09 <dakshiegoel@gmail.com>
2024-03-02 22:58:25 +0530

    nav fix

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,66 +1,53 @@
+
 "use client";
-import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '');
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
+import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-function page() {
+const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
 
-    const cartItems = [
-        {
-            "name": "Espresso Bean Dark Chocolate",
-            "bgColor": "bg-green-200",
-            "mainImage": "./chocochip-brownie.png",
-            "quantity": 4
-        },
-        {
-            "name": "Adventure Trail Mix Fusion",
-            "bgColor": "bg-pink-200",
-            "mainImage": "./tropical-coconut.png",
-            "quantity": 2
-        }
-    ]
+function PaymentForm() {
+    const stripe = useStripe();
+    const elements = useElements();
 
-    const handleCheckout = async () => {
-        const stripe = await stripePromise;
-        if (!stripe) {
-            // Handle the case where Stripe failed to initialize (e.g., log the error or notify the user)
-            console.error('Stripe has not been initialized');
-            return;
-        }
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const cardElement = elements?.getElement("card");
 
         try {
-            const response = await fetch('/api', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ items: cartItems }), // Replace cartItems with your cart items array
+            if (!stripe || !cardElement) return null;
+            const { data } = await axios.post("/api/create-payment-intent", {
+                data: { amount: 89 },
             });
-            const session = await response.json();
+            const clientSecret = data;
 
-            const result = await stripe.redirectToCheckout({
-                sessionId: session.sessionId,
+            await stripe?.confirmCardPayment(clientSecret, {
+                payment_method: { card: cardElement },
             });
-
-            if (result.error) {
-                // Inform the user if there was an error
-                alert(result.error.message);
-            }
         } catch (error) {
-            // Handle any other errors (e.g., network error, etc.)
-            console.error('Error during checkout process', error);
+            console.log(error);
         }
     };
 
-
-
     return (
-        <div>
-            <h1 className="text-5xl">Stripe Checkout</h1>
-            <button onClick={handleCheckout}>checkout</button>
-        </div>
+        <form onSubmit={onSubmit}>
+            <CardElement />
+            <button type="submit">Submit</button>
+        </form>
     );
-};
+}
 
-export default page;
+function page() {
+    return (
+        <Elements stripe={stripePromise}>
+            <PaymentForm />
+        </Elements>
+    )
+}
+
+export default page
